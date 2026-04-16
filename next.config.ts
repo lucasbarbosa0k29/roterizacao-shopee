020aeffd37372a59cd99cf6bdee505140b5b77c1
Lucas Gabriel <lucasbarbosa0k29@gmail.com>
2026-04-15 22:21:46 -0300

    fix: reduzir memória build arcgis

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@arcgis/core"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals ?? [];
+      config.externals.push(/^@arcgis\/core(\/.*)?$/);
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
